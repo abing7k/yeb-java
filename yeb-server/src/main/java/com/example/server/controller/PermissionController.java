2bef45e695bd7a40b7b76022013a59b913c5110a
hanbing <742608569@qq.com>
2022-10-25 15:28:53 +0800

    权限组菜单查询功能实现 p94 java-23

--- yeb-server/src/main/java/com/example/server/controller/PermissionController.java
+++ yeb-server/src/main/java/com/example/server/controller/PermissionController.java
@@ -1,14 +1,21 @@
 package com.example.server.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.server.pojo.Menu;
+import com.example.server.pojo.MenuRole;
 import com.example.server.pojo.Role;
 import com.example.server.pojo.RespBean;
+import com.example.server.service.IMenuRoleService;
+import com.example.server.service.IMenuService;
 import com.example.server.service.IRoleService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @PROJECT_NAME: yeb-java
@@ -20,7 +27,11 @@ import java.util.List;
 @RequestMapping("/system/basic/pos")
 public class PermissionController {
     @Autowired
-    public IRoleService roleService;
+    private IRoleService roleService;
+    @Autowired
+    private IMenuService menuService;
+    @Autowired
+    private IMenuRoleService menuRoleService;
 
     @ApiOperation(value = "获取职位信息")
     @GetMapping("/getAllRole")
@@ -32,7 +43,7 @@ public class PermissionController {
     @PostMapping("/addRole")
     public RespBean addRole(@RequestBody Role role) {
         if (!role.getName().startsWith("ROLE_")) {
-            role.setName("ROLE_"+role.getName());
+            role.setName("ROLE_" + role.getName());
         }
         if (roleService.save(role)) {
             return RespBean.success("添加成功");
@@ -69,5 +80,21 @@ public class PermissionController {
         } else {
             return RespBean.error("删除失败");
         }
+    }
+
+    @ApiOperation(value = "查询所有菜单")
+    @GetMapping("/menus")
+    public List<Menu> getAllMenus() {
+        return menuService.getAllMenus();
+    }
+
+    @ApiOperation(value = "根据角色id查询菜单id")
+    @GetMapping("/mid/{rid}")
+    public List<Integer> getMidByRid(@PathVariable Integer rid){
+        return menuRoleService.list(new QueryWrapper<MenuRole>()
+                .eq("rid",rid))
+                .stream()
+                .map(MenuRole::getMid)
+                .collect(Collectors.toList());
     }
 }
